6968b31a73a9fe8d662f8578b1e45cd1bbe0afda
Nayeem Hasan <nayeemhasan@Nayeems-MacBook-Pro.local>
2020-08-25 06:18:30 +0600

    create react context api

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,6 @@
 export const initialState = {
   basket: [],
-  user:null,
+  user: null,
 };
 
 const reducer = (state, action) => {
@@ -8,10 +8,10 @@ const reducer = (state, action) => {
   switch (action.type) {
     case 'ADD_TO_BASKET':
       //logic for ading item to basket
-      return { 
-          ...state,
-          basket:
-        };
+      return {
+        ...state,
+        basket: [...state.basket, action.item],
+      };
       break;
     case 'REMOVE_FROM_BASKET':
       //logic for Removing item from basket
