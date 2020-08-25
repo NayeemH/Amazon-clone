fd9d12d8570c0a3b877d8e8da5a6ab0f220908e5
Nayeem Hasan <nayeemhasan@Nayeems-MacBook-Pro.local>
2020-08-26 02:15:38 +0600

    working on login page

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,15 @@
 import firebase from 'firebase';
 
-const firebaseApp =firebase.initializeApp(
-  {
-    apiKey: 'AIzaSyAeqWMT1SILAN0klPevBxjj25JnGTm0l7Y',
-    authDomain: 'ekhenei.firebaseapp.com',
-    databaseURL: 'https://ekhenei.firebaseio.com',
-    projectId: 'ekhenei',
-    storageBucket: 'ekhenei.appspot.com',
-    messagingSenderId: '983600411645',
-    appId: '1:983600411645:web:1b8c9e565f8dd9f79bdf94',
-    measurementId: 'G-LLEGRM6VJF',
-  };
-) 
-
+const firebaseApp = firebase.initializeApp({
+  apiKey: 'AIzaSyAeqWMT1SILAN0klPevBxjj25JnGTm0l7Y',
+  authDomain: 'ekhenei.firebaseapp.com',
+  databaseURL: 'https://ekhenei.firebaseio.com',
+  projectId: 'ekhenei',
+  storageBucket: 'ekhenei.appspot.com',
+  messagingSenderId: '983600411645',
+  appId: '1:983600411645:web:1b8c9e565f8dd9f79bdf94',
+  measurementId: 'G-LLEGRM6VJF',
+});
 
 const auth = firebase.auth();
 
